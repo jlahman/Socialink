5e900c4b6d5a3304c2492808d66fd53f4b972c05
jlahman <JustinLahmanCS@gmail.com>
2022-04-11 20:18:45 -0400

    Added Escaping to parameters

--- AvatarGrabberApp.py
+++ AvatarGrabberApp.py
@@ -8,10 +8,12 @@ grabbableSites = ["github"]
 
 @app.route("/api/v1/Avatars/<string:sitename>/<string:username>", methods=['GET'])
 def githubAvatarEndpoint(sitename, username):
+	sitename = escape(sitename)
+	username = escape(username)
 	if(sitename.lower() not in grabbableSites):
 		abort(400)
 	grabber = AvatarGrabber.AvatarGrabber()
-	url = grabber.grabAvatarGithub(escape(username))
+	url = grabber.grabAvatarGithub(username)
 	if(url == "Not Found"):
 		abort(404)
 	return jsonify({"src_url" : url})
